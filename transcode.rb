b67f16759529e5d672742ac3b63eb6cf4ef4567f
Alex Du Bois <alex.dubois@chaione.com>
2017-08-02 19:47:42 -0500

    Add Transcode HEAD

--- transcode.rb
+++ transcode.rb
@@ -3,6 +3,7 @@ class Transcode < Formula
   homepage "https://github.com/alextall/Transcode"
   url "https://github.com/alextall/Transcode/archive/v1.4.tar.gz"
   sha256 "db3c80a735a9ab1571dd3f0f7cf28b4531202c34db9c1c539fb9b7e111bf572d"
+  head "https://github.com/alextall/Transcode.git"
 
   def install
     bin.install "transcode.sh"
