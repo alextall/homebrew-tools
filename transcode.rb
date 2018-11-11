3131632448d9d490289b972ab9cb2f16b3bb298f
Alex <alextall@users.noreply.github.com>
2018-11-11 10:47:19 -0600

    Update transcode to v1.6.1

--- transcode.rb
+++ transcode.rb
@@ -1,8 +1,8 @@
 class Transcode < Formula
   desc ""
   homepage "https://github.com/alextall/Transcode"
-  url "https://github.com/alextall/Transcode/archive/v1.6.tar.gz"
-  sha256 "76ec8cd3e21654237903058db0b1a6666e94f1faf5dbade4679827c3327587b1"
+  url "https://github.com/alextall/Transcode/archive/v1.6.1.tar.gz"
+  sha256 "fd8cd7d94eaf259289507e8823949457c4d3a5767e3ee01a8119ef072994b892"
   head "https://github.com/alextall/Transcode.git"
 
   def install
